28e3ba69625810da3fbb909f8f9430b2cfd97de4
CodyGuo <CodyGuo@aliyun.com>
2020-04-12 01:50:29 +0800

    Update godaemon.go
    panic: runtime error: slice bounds out of range [1:0]

--- godaemon.go
+++ godaemon.go
@@ -7,18 +7,14 @@ import (
 	"os/exec"
 )
 
-var godaemon = flag.Bool("d", false, "run app as a daemon with -d=true or -d true.")
-
 func init() {
-	if !flag.Parsed() {
-		flag.Parse()
-	}
+	goDaemon := flag.Bool("d", false, "run app as a daemon with -d=true.")
+	flag.Parse()
 
-	if *godaemon {
-		cmd := exec.Command(os.Args[0], flag.Args()[1:]...)
+	if *goDaemon {
+		cmd := exec.Command(os.Args[0], flag.Args()...)
 		cmd.Start()
 		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
-		*godaemon = false
 		os.Exit(0)
 	}
 }
