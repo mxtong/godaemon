7103c83efa3b4a0a5cc09746a80a1ac60c3fc41c
CodyGuo <CodyGuo@aliyun.com>
2020-04-12 01:54:03 +0800

    Update godaemon.go

--- godaemon.go
+++ godaemon.go
@@ -13,7 +13,10 @@ func init() {
 
 	if *goDaemon {
 		cmd := exec.Command(os.Args[0], flag.Args()...)
-		cmd.Start()
+		if err := cmd.Start(); err != nil {
+			fmt.Printf("start %s failed, error: %v\n", os.Args[0])
+			os.Exit(1)
+		}
 		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
 		os.Exit(0)
 	}
