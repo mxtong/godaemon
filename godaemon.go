ea9c3189b9da1f5863a0021480f05b5980a90050
mxtong <manxiaotong@qq.com>
2021-01-29 12:39:46 +0800

    Update godaemon.go

--- godaemon.go
+++ godaemon.go
@@ -8,10 +8,10 @@ import (
 )
 
 func init() {
-	goDaemon := flag.Bool("d", false, "run app as a daemon with -d=true.")
+	Daemon := flag.Bool("d", false, "run program as a daemon with -d=true.")
 	flag.Parse()
 
-	if *goDaemon {
+	if *Daemon {
 		cmd := exec.Command(os.Args[0], flag.Args()...)
 		if err := cmd.Start(); err != nil {
 			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
