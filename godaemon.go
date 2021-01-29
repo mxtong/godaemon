c76742bd8c6467e492892fb9688a6d5430f72fb1
mxtong <manxiaotong@qq.com>
2021-01-29 13:52:43 +0800

    Update godaemon.go

--- godaemon.go
+++ godaemon.go
@@ -7,11 +7,22 @@ import (
 	"os/exec"
 )
 
-func init() {
-	Daemon := flag.Bool("start", false, "run program as a daemon with start.")
-	flag.Parse()
+func usage() {
+    fmt.Fprintf(os.Stderr, `Options:
+start : start the program as a daemon
+`)
+}
 
-	if *Daemon {
+func init() {
+	Daemon := ""
+	flag.Usage = usage
+	flag.Parse()
+	flag.Usage()
+	if len(flag.Args()) > 0 {
+		Daemon = flag.Args()[0]
+	}
+
+	if Daemon == "start" {
 		cmd := exec.Command(os.Args[0], flag.Args()...)
 		if err := cmd.Start(); err != nil {
 			fmt.Printf("start %s failed, error: %v\n", os.Args[0], err)
@@ -19,5 +30,8 @@ func init() {
 		}
 		fmt.Printf("%s [PID] %d running...\n", os.Args[0], cmd.Process.Pid)
 		os.Exit(0)
+	} else {
+		fmt.Println("Invalid parameter")
+		os.Exit(1)
 	}
 }
